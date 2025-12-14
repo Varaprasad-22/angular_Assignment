21727de4a4f6b9ef345223df252a7b5b0388093f
SriVaishnavi2110 <yadavalli.srivaishnavi@example.com>
2025-12-15 03:23:59 +0530

    modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Register } from './auth/register/register';
-import { Login } from './auth/login/login';
-import { SearchFlights } from './flights/search-flights/search-flights';
+import { RegisterComponent } from './auth/register/register';
+import { LoginComponent } from './auth/login/login';
+import { SearchFlightsComponent } from './flights/search-flights/search-flights';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'register', component: Register },
-  { path: 'login', component: Login },
-  { path: 'search-flights', component: SearchFlights }
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'search-flights', component: SearchFlightsComponent }
 ];
 
