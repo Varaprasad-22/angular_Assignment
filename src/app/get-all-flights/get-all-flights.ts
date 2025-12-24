45b69a6ecc8143900bf968e267018b36efd019da
Varaprasad-22 <virupavaraprasad22@gmail.com>
2025-12-24 14:45:42 +0530

    rectified the allflights uis

--- src/app/get-all-flights/get-all-flights.ts
+++ src/app/get-all-flights/get-all-flights.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,ChangeDetectorRef } from '@angular/core';
 import { FlightService } from '../services/flight';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-get-all-flights',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './get-all-flights.html',
   styleUrl: './get-all-flights.css',
 })
@@ -11,13 +12,16 @@ export class GetAllFlights {
 
   flights:any[]=[];
   errorMessage='';
-  constructor(private flightService:FlightService){
+  constructor(private flightService:FlightService,
+    private cdr:ChangeDetectorRef
+  ){
 this.getAllFlights();
   }
   getAllFlights():void{
     this.flightService.getAllFlights().subscribe({
       next:(response:any[])=>{
         this.flights=response;
+        this.cdr.detectChanges();
       },
       error:(err)=>{
           this.errorMessage = err.error?.message || 'Failed to load flights';
