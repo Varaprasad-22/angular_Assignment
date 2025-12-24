6618c609a0ae9b9f668a40f4631636b1c8b8d484
Varaprasad-22 <virupavaraprasad22@gmail.com>
2025-12-24 16:19:40 +0530

    password expire updates

--- src/app/expired-password/expired-password.ts
+++ src/app/expired-password/expired-password.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component,ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../services/auth';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expired-password',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './expired-password.html',
   styleUrl: './expired-password.css',
 })
@@ -12,7 +14,43 @@ export class ExpiredPassword {
 
   errorMessage='';
   successMessage='';
+
+  expiredForm!:FormGroup
   constructor(
-    private authService:AuthService
-  ){}
+    private authService:AuthService,
+    private fb:FormBuilder,
+    private route:Router
+  ){
+    this.expiredForm=this.fb.group({
+    name: ['',Validators.required],
+    oldPassword:['',Validators.required],
+    newPassword:['',Validators.required],
+    confirmPassword:['',Validators.required]
+  });
+  }
+
+  submit(){
+    this.errorMessage='';
+    this.successMessage='';
+
+    if(this.expiredForm.invalid){
+      return;
+    }
+
+    if(this.expiredForm.value.newPassword!==this.expiredForm.value.confirmPassword){
+      this.errorMessage="Password did not match"
+      return;
+
+    }
+    this.authService.expiredPasswordChange(this.expiredForm.value).subscribe({
+      next:(response:any)=>{
+        this.successMessage=response.message||"Password Updated SuccesFully Please Login";
+        setTimeout(() => this.route.navigate(['/login']), 1500);
+      },
+      error:(err)=>{
+        this.errorMessage=err.error;
+
+      }
+    });
+  }
 }
